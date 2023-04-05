6f0ed0dd661b4bede2a3d8a0a96f62d83a3f525d
Hrishikesh Gadekar <55317394+HrishikeshGadekar@users.noreply.github.com>
2023-04-05 15:45:58 +0530

    Update LoggingExchangeFilterFunction.java

--- LoggingExchangeFilterFunction.java
+++ LoggingExchangeFilterFunction.java
@@ -1,109 +1,81 @@
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.core.io.buffer.DataBuffer;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.client.reactive.ClientHttpRequest;
-import org.springframework.http.client.reactive.ClientHttpResponse;
-import org.springframework.http.server.reactive.ServerHttpRequest;
-import org.springframework.http.server.reactive.ServerHttpResponse;
-import org.springframework.lang.Nullable;
-import org.springframework.util.StringUtils;
-import org.springframework.web.reactive.function.client.ExchangeFunction;
-import org.springframework.web.server.ServerWebExchange;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
-import reactor.util.annotation.NonNull;
-
-import java.nio.charset.StandardCharsets;
-import java.time.Duration;
-import java.time.Instant;
-
-public class CustomLoggingFilter implements ExchangeFunction {
-
-    private static final Logger logger = LoggerFactory.getLogger(CustomLoggingFilter.class);
-
-    private final ExchangeFunction delegate;
-
-    public CustomLoggingFilter(ExchangeFunction delegate) {
-        this.delegate = delegate;
-    }
-
-    @Override
-    public Mono<ClientHttpResponse> exchange(ClientHttpRequest request) {
-        logRequest(request);
-        Instant requestTime = Instant.now();
-        return delegate.exchange(request)
-                       .doOnNext(response -> logResponse(request.getMethod(), request.getURI()
-                                                                                     .toString(), request.getHeaders(), response,
-                               requestTime));
-    }
-
-    private void logRequest(ClientHttpRequest request) {
-        HttpMethod method = request.getMethod();
-        String url = request.getURI()
-                            .toString();
-        HttpHeaders headers = request.getHeaders();
-        Object requestBody = null;
-        if (logger.isDebugEnabled()) {
-            requestBody = request.getBody();
-        }
-        logger.debug("[Request] Method: {}, URL: {}, Headers: {}, Body: {}", method, url, headers, requestBody);
-    }
-
-    private void logResponse(HttpMethod method, String url, HttpHeaders requestHeaders, @Nullable ClientHttpResponse response, Instant requestTime) {
-        Instant responseTime = Instant.now();
-        long durationMs = Duration.between(requestTime, responseTime)
-                                  .toMillis();
-
-        if (response != null) {
-            HttpStatus status = response.getStatusCode();
-            HttpHeaders headers = response.getHeaders();
-            Object responseBody = null;
-            if (logger.isDebugEnabled()) {
-                responseBody = readResponseBody(response);
+WebClient webClient = WebClient.builder()
+        .baseUrl("http://example.com")
+        .filter(ExchangeFilterFunctions.ofRequestProcessor(request -> {
+            if (log.isTraceEnabled()) {
+                log.trace("Request: {} {} {} {}",
+                        request.method(),
+                        request.url(),
+                        request.headers(),
+                        request.bodyToMono(String.class));
+            } else {
+                log.debug("Request: {} {}",
+                        request.method(),
+                        request.url());
             }
-            logger.debug("[Response] Method: {}, URL: {}, Status: {}, Headers: {}, Duration: {} ms, ResponseBody: {}", method,
-                    url, status, headers, durationMs, responseBody);
-        } else {
-            logger.debug("[Response] Method: {}, URL: {}, Status: [NO RESPONSE], Headers: {}, Duration: {} ms", method, url,
-                    requestHeaders, durationMs);
-        }
-    }
 
-    private Object readResponseBody(ClientHttpResponse response) {
-        Flux<DataBuffer> flux = response.getBody();
-        StringBuilder builder = new StringBuilder();
-        flux.subscribe(buffer -> {
-            byte[] bytes = new byte[buffer.readableByteCount()];
-            buffer.read(bytes);
-            builder.append(new String(bytes, StandardCharsets.UTF_8));
-        });
-        return builder.toString();
-    }
+            return request;
+        }))
+        .filter((clientRequest, next) -> {
+            long startTime = System.currentTimeMillis();
 
-    @Override
-    public Mono<ClientHttpResponse> exchange(ServerWebExchange exchange) {
-        ServerHttpRequest request = exchange.getRequest();
-        logRequest(request);
-        Instant requestTime = Instant.now();
-        return delegate.exchange(exchange)
-                       .doOnNext(response -> logResponse(request.getMethod(), request.getURI()
-                                                                                     .toString(), request.getHeaders(), response,
-                               requestTime));
-    }
+            return next.exchange(clientRequest)
+                    .doOnSuccess(clientResponse -> {
+                        long duration = System.currentTimeMillis() - startTime;
 
-    private void logRequest(ServerHttpRequest request) {
-        HttpMethod method = request.getMethod();
-        String url = request.getURI()
-                            .toString();
-        HttpHeaders headers = request.getHeaders();
-        Object requestBody = null;
-        if (logger.isDebugEnabled()) {
-            requestBody = request.getBody();
-        }
-        logger.debug("[Request] Method: {}, URL: {}, Headers: {}, Body: {}", method, url, headers, requestBody);
-    }
+                        if (log.isTraceEnabled()) {
+                            log.trace("Response: {} {} {} {} ({} ms)",
+                                    clientResponse.statusCode(),
+                                    clientResponse.statusCode().getReasonPhrase(),
+                                    clientResponse.headers().asHttpHeaders(),
+                                    clientResponse.bodyToMono(String.class),
+                                    duration);
+                        } else {
+                            log.debug("Response: {} {} ({} ms)",
+                                    clientResponse.statusCode(),
+                                    clientResponse.statusCode().getReasonPhrase(),
+                                    duration);
+                        }
+                    })
+                    .doOnError(throwable -> {
+                        long duration = System.currentTimeMillis() - startTime;
 
-}
+                        log.error("Error making request: {} ({} ms)", throwable.getMessage(), duration);
+                    });
+        })
+        .build();
+
+
+// No check for Trace or Debug level
+
+WebClient webClient = WebClient.builder()
+        .baseUrl("http://example.com")
+        .filter(ExchangeFilterFunctions.ofRequestProcessor(request -> {
+            log.trace("Request: {} {} {} {}",
+                    request.method(),
+                    request.url(),
+                    request.headers(),
+                    request.bodyToMono(String.class));
+
+            return request;
+        }))
+        .filter((clientRequest, next) -> {
+            long startTime = System.currentTimeMillis();
+
+            return next.exchange(clientRequest)
+                    .doOnSuccess(clientResponse -> {
+                        long duration = System.currentTimeMillis() - startTime;
+
+                        log.trace("Response: {} {} {} {} ({} ms)",
+                                clientResponse.statusCode(),
+                                clientResponse.statusCode().getReasonPhrase(),
+                                clientResponse.headers().asHttpHeaders(),
+                                clientResponse.bodyToMono(String.class),
+                                duration);
+                    })
+                    .doOnError(throwable -> {
+                        long duration = System.currentTimeMillis() - startTime;
+
+                        log.error("Error making request: {} ({} ms)", throwable.getMessage(), duration);
+                    });
+        })
+        .build();
