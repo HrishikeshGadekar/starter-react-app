b1c98ee846429afb35bb57e57831abfc87317c55
Hrishikesh Gadekar <55317394+HrishikeshGadekar@users.noreply.github.com>
2023-04-05 15:51:55 +0530

    Update LoggingExchangeFilterFunction.java

--- LoggingExchangeFilterFunction.java
+++ LoggingExchangeFilterFunction.java
@@ -1,6 +1,46 @@
 WebClient webClient = WebClient.builder()
         .baseUrl("http://example.com")
         .filter(ExchangeFilterFunctions.ofRequestProcessor(request -> {
+            log.trace("Request: {} {} {} {}",
+                    request.method(),
+                    request.url(),
+                    request.headers(),
+                    DataBufferUtils.join(request.body(BodyExtractors.toDataBuffers())).flatMap(dataBuffer -> {
+                        byte[] bytes = new byte[dataBuffer.readableByteCount()];
+                        dataBuffer.read(bytes);
+                        DataBufferUtils.release(dataBuffer);
+                        return Mono.just(new String(bytes, StandardCharsets.UTF_8));
+                    }).orElse(""));
+            return request;
+        }))
+        .filter((clientRequest, next) -> {
+            long startTime = System.currentTimeMillis();
+
+            return next.exchange(clientRequest)
+                    .doOnSuccess(clientResponse -> {
+                        long duration = System.currentTimeMillis() - startTime;
+
+                        log.trace("Response: {} {} {} {} ({} ms)",
+                                clientResponse.statusCode().value(),
+                                HttpStatus.valueOf(clientResponse.statusCode().value()).getReasonPhrase(),
+                                clientResponse.headers().asHttpHeaders(),
+                                clientResponse.bodyToMono(String.class),
+                                duration);
+                    })
+                    .doOnError(throwable -> {
+                        long duration = System.currentTimeMillis() - startTime;
+
+                        log.error("Error making request: {} ({} ms)", throwable.getMessage(), duration);
+                    });
+        })
+        .build();
+
+
+// 
+// 
+WebClient webClient = WebClient.builder()
+        .baseUrl("http://example.com")
+        .filter(ExchangeFilterFunctions.ofRequestProcessor(request -> {
             if (log.isTraceEnabled()) {
                 log.trace("Request: {} {} {} {}",
                         request.method(),
